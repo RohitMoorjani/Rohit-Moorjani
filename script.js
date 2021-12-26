ff52d3f9c5a9fe48357118725472282a0e8c3432
RohitMoorjani <roh123890@icloud.com>
2021-12-26 23:25:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(e){
+//     $(window).scroll(function(e){
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }
+//         else{
+//         }
+//     });
+// });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+
+    });
+});
