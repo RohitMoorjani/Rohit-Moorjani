286dd4e486e41a4e7ea43025c4889bd94cac1f0a
RohitMoorjani <roh123890@icloud.com>
2022-01-11 20:47:38 +0530

    color change in services, removed PM from typing

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     });
 
     var typed=new Typed(".typing",{
-        strings: ["Developer", "Product Manager", "YouTuber", "Freelancer"],
+        strings: ["Developer", "YouTuber", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
